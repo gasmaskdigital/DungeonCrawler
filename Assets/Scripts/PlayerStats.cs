c99a8df829b6023e8eb2b2647d11cd9987d35006
GamersGonnaGame <60356506+GamersGonnaGame@users.noreply.github.com>
2026-07-04 10:15:40 +0100

    removed debug health display

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -87,7 +87,7 @@ public class PlayerStats : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Current Health: " + currentHealth);
+        //Debug.Log("Current Health: " + currentHealth);
     }
 
     public void AddToXP(int xpReward)
