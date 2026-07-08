8a4e43edf87771c032006e5e27c627b29cdfc815
GamersGonnaGame <60356506+GamersGonnaGame@users.noreply.github.com>
2026-07-08 16:31:43 +0100

    Fog of War
    Fog of War added to the minimap

---
+++ Assets/Scripts/tileMapScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tileMapScript : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    [SerializeField] RawImage mapIcon;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mapIcon = GetComponentInChildren<RawImage>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player) mapIcon.enabled = false;
+    }
+}
