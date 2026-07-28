8c86235f7584c11a74de6e0016306e9141f8f0e7
Harrick22 <Jayrhysfox@gmail.com>
2026-07-28 21:20:02 +0100

    Button Press and Container input

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -26,7 +26,7 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        txtHealth.text = "Health: " + PlayerStats.currentHealth;
+        txtHealth.text = " " + PlayerStats.currentHealth;
         updateStatDisplay();
     }
 
